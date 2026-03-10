d664b2b0e81c689d2f341f432be53d23496993f0
Shashank Shree Neupane <shashank@neupane.com.np>
2026-03-10 15:58:37 +0545

    secret key and production update

--- watchdog/settings.py
+++ watchdog/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,10 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('WD_KEY', 'q_#-6*gvpbjz7x6l0f1u44m=xpd!gl93p7%m(jbqyd6+&^2u==')
+SECRET_KEY = os.environ.get('WD_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Setting ENVIRONMENT='production' in production will turn off DEBUG and switch to MySQL.
+ENVIRONMENT = os.environ.get('ENVIRONMENT', 'development')
+DEBUG = ENVIRONMENT != 'production'
 
 ALLOWED_HOSTS = ['watchdognepal.com', 'www.watchdognepal.com','127.0.0.1']
 
