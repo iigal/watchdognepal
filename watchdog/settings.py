021bde069f48f7f86ae51975a454f2512bf66101
Shashank Shree Neupane <shashank@neupane.com.np>
2026-03-10 16:15:11 +0545

    added whitenoise

--- watchdog/settings.py
+++ watchdog/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -148,6 +149,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
 
 # Optional but good practice:
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
