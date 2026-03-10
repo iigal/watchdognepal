f67a88158f50b638197c03ad603c3c064424845f
Shashank Shree Neupane <shashank@neupane.com.np>
2026-03-10 15:32:00 +0545

    corrected the bootstrap

--- watchdog/settings.py
+++ watchdog/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'accounts',
     'widget_tweaks',
     'django_bootstrap5',
+    "crispy_forms",
+    "crispy_bootstrap5",
 ]
 
 MIDDLEWARE = [
@@ -151,3 +153,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
