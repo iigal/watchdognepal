46c3fd856523df749f894b8181acf10a28e3f9f0
Shashank Shree Neupane <shashank@neupane.com.np>
2026-03-16 14:20:27 +0545

    Update SMTP settings for cPanel webmail and set default from email to no-reply@watchdognepal.com

--- watchdog/settings.py
+++ watchdog/settings.py
@@ -233,13 +233,13 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+    EMAIL_HOST = os.environ.get('EMAIL_HOST', 'mail.watchdognepal.com')
     EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
     EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 
-DEFAULT_FROM_EMAIL = 'Watchdog Nepal <noreply@watchdognepal.com>'
+DEFAULT_FROM_EMAIL = 'Watchdog Nepal <no-reply@watchdognepal.com>'
 
 # SMTP Configuration Check (Production only)
 if not DEBUG and EMAIL_BACKEND == 'django.core.mail.backends.smtp.EmailBackend':
